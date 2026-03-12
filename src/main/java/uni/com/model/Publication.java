0d0757250b2773fafecb55530266e9918064f052
Valeriia <valeriaokh@gmail.com>
2026-03-12 20:36:08 +0100

    Implement Publication entity

--- src/main/java/uni/com/model/Publication.java
+++ src/main/java/uni/com/model/Publication.java
@@ -1,5 +1,41 @@
 package uni.com.model;
 
-public class Publication {
+import java.io.Serializable;
 
+public class Publication implements Serializable {
+    private String title;
+    private int year;
+
+    //constructors
+    public Publication() {
+    }
+
+    public Publication(String title, int year) {
+        this.title = title;
+        this.year = year;
+    }
+
+    //getters
+    public String getTitle() {
+        return title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    //setters
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return "Publication{" + "title='" + title + '\'' + ", year=" + year + '}';
+    }
 }
