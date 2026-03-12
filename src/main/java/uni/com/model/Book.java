e6664db173f980e28715cc70f38080a5a26d9996
Valeriia <valeriaokh@gmail.com>
2026-03-12 20:41:47 +0100

    Implement Book entity which extends Publication

--- src/main/java/uni/com/model/Book.java
+++ src/main/java/uni/com/model/Book.java
@@ -1,4 +1,49 @@
 package uni.com.model;
 
-public class Book {
+public class Book extends Publication {
+    private String author;
+    private String publisher;
+    private String genre;
+
+    //constructors
+    public Book() {
+    }
+
+    public Book(String title, int year, String author, String publisher, String genre) {
+        super(title, year);
+        this.author = author;
+        this.publisher = publisher;
+        this.genre = genre;
+    }
+
+    //getters
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    //setters
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    @Override
+    public String toString() {
+        return "Book: " + "title='" + getTitle() + '\'' + ", year=" + getYear() + ", author='" + author + '\'' + ", publisher='" + publisher + '\'' + ", genre='" + genre + '\'';
+    }
 }
